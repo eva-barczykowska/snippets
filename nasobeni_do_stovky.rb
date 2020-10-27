de401d3bf05df1493e910b8ba802c1eeecb32eb1
eva-barczykowska <eva.barczykowska@gmail.com>
2020-10-27 12:06:00 +0100

    fixed hard coding of counter

--- nasobeni_do_stovky.rb
+++ nasobeni_do_stovky.rb
@@ -1,36 +1,32 @@
 puts "Hello! What is your name?"
 name = gets.chomp
-
 puts " "
-
-puts "Welcome, #{name}."
+puts "Welcome, #{name}! Let's practise adding to 100."
 puts " "
-puts "Let's practise adding to 100."
+counter = 3
+puts "You need to do only #{counter} examples today. Good luck!"
 puts " "
-puts "You need to do only 4 examples today. Good luck!"
+praise_words = ["Awesome", "Fantastic", "Very good", "Doing well", "Muy bien", "Super", "Great job!", "You're on a roll!", "Aby tak dalej!"]
 
-puts " "
-praise_words = ["Skvele", "Fantastic", "Very good", "Doing well"]
-
-counter = 4  #I want him to do that many examples. Practise makes perfect! Of course when HE does it, I will increase the counter, hehe.
+counter = 3  #I want him to do that many examples. Practise makes perfect! Of course when HE does it, I will increase the counter, hehe.
 while counter > 0
   a = 1 + rand(50)
   b = 1 + rand(50)
-  def nasobeni(a,b)
+  def add(a,b)
     res = a + b
   end
-  res = nasobeni(a,b)
+  res = add(a,b)
   puts "How much is #{a} and #{b}?"
   guess = gets.chomp.to_i
-  until guess == res
+  until guess == res   # this is where my loop starts
     puts "Nope, try again"
     puts "How much is #{a} and #{b}?"
     guess = gets.chomp.to_i
     end
-  if guess == res
+  if guess == res # this is conditional flow
      counter -= 1
        if counter != 0 && counter != 1
-         puts "Very good! Now you need to do #{counter} examples."
+         puts "Very good! #{counter} examples are left."
        elsif counter == 1
          puts praise_words.sample + ", you're almost there! This is the last example!"
        else
